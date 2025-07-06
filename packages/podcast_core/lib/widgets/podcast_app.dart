0c4520b61302ec83bef9c8f0fdffd794abfddbb2
lohnn <johannes@lohnn.se>
2025-07-06 18:27:09 +0300

    Now finally I should support the languages properly

--- packages/podcast_core/lib/widgets/podcast_app.dart
+++ packages/podcast_core/lib/widgets/podcast_app.dart
@@ -2,15 +2,21 @@ import 'package:accessibility_tools/accessibility_tools.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:podcast_core/gen/strings.g.dart';
 import 'package:podcast_core/providers/episode_color_scheme_provider.dart';
 
-class PodcastApp extends HookConsumerWidget {
+class PodcastApp extends StatefulHookConsumerWidget {
   final Widget child;
 
   const PodcastApp({super.key, required this.child});
 
+  @override
+  ConsumerState<PodcastApp> createState() => _PodcastAppState();
+}
+
+class _PodcastAppState extends ConsumerState<PodcastApp> {
   Future<ColorScheme> _loadDefaultColorScheme(Brightness brightness) async {
     const icon = AssetImage(
       'assets/icons/app_icon.webp',
@@ -23,7 +29,13 @@ class PodcastApp extends HookConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+    LocaleSettings.useDeviceLocale();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final brightness = MediaQuery.platformBrightnessOf(context);
     final iconColorSchemeFuture = useMemoized(() {
       return _loadDefaultColorScheme(brightness);
@@ -36,18 +48,25 @@ class PodcastApp extends HookConsumerWidget {
 
     if (colorScheme ?? iconColorScheme case final colorScheme?) {
       return TranslationProvider(
-        child: MaterialApp(
-          theme: ThemeData.light().applyPodcastTheme(colorScheme),
-          darkTheme: ThemeData.dark().applyPodcastTheme(colorScheme),
-          debugShowCheckedModeBanner: false,
-          builder: kDebugMode
-              ? (_, child) => AccessibilityTools(
-                  logLevel: LogLevel.none,
-                  buttonsAlignment: ButtonsAlignment.bottomLeft,
-                  child: child,
-                )
-              : null,
-          home: child,
+        child: Builder(
+          builder: (context) {
+            return MaterialApp(
+              locale: TranslationProvider.of(context).flutterLocale,
+              supportedLocales: AppLocaleUtils.supportedLocales,
+              localizationsDelegates: GlobalMaterialLocalizations.delegates,
+              theme: ThemeData.light().applyPodcastTheme(colorScheme),
+              darkTheme: ThemeData.dark().applyPodcastTheme(colorScheme),
+              debugShowCheckedModeBanner: false,
+              builder: kDebugMode
+                  ? (_, child) => AccessibilityTools(
+                      logLevel: LogLevel.none,
+                      buttonsAlignment: ButtonsAlignment.bottomLeft,
+                      child: child,
+                    )
+                  : null,
+              home: widget.child,
+            );
+          },
         ),
       );
     }
